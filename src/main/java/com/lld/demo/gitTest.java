7f86dee1eae8214b2bc06b8a026f1f936520dc9f
lld <3271663714@qq.com>
2022-08-20 17:11:30 +0800

    push commit

--- src/main/java/com/lld/demo/gitTest.java
+++ src/main/java/com/lld/demo/gitTest.java
@@ -11,5 +11,6 @@ public class gitTest {
         System.out.println("hello,git2");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
